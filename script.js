3c8407497672114f4a05f1885936ff91545dca0c
obada <obadajaber10@gmail.com>
2026-06-20 10:01:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ let prevBtn = document.getElementById("prev-btn")
 let nextBtn = document.getElementById("next-btn")
 let playBtn = document.getElementById("play-btn");
 
+let dot0 = document.getElementById("dot-0")
+let dot1 = document.getElementById("dot-1")
+let dot2 = document.getElementById("dot-2");
+
+let playerScoreText = document.getElementById("player-score")
+let computerScoreText = document.getElementById("computer-score")
+let roundNumberText = document.getElementById("round-number");
+
+let roundResult = document.getElementById("round-result")
+let roundResultText = document.getElementById("round-result-text");
+
+let playerBattleImg = document.getElementById("player-battle-img")
+let computerBattleImg = document.getElementById("computer-battle-img");
+
+let playerPlaceholder = document.getElementById("player-placeholder")
+let computerPlaceholder = document.getElementById("computer-placeholder");
 
