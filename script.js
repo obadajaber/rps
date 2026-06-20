eca381b52c1b4ec5d18d67ae5d1176bae84baf9b
obada <obadajaber10@gmail.com>
2026-06-20 09:39:24 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("hello ");
+let sliderTrack = document.getElementById("slider-track");
+
+let prevBtn = document.getElementById("prev-btn")
+let nextBtn = document.getElementById("next-btn")
+let playBtn = document.getElementById("play-btn");
+
+
